703c7c7f8f2f741b163ece4cd5e4e78f6fbb117d
smartiri <rasa7825@gmail.com>
2024-10-30 09:41:13 +0100

    validation

--- src/the_hooks/useStepForm.js
+++ src/the_hooks/useStepForm.js
@@ -16,14 +16,31 @@ function useStepForm() {
     setFormData((fd) => (!storedData ? fd : JSON.parse(storedData)));
   }, []);
 
-  const setStep1 = (name, surname) =>
-    setFormData((fd) => ({ ...fd, name, surname, step: 2 }));
+  const setStep1 = (name, surname) => {
+    if (name !== "" && surname !== "") {
+      return setFormData((fd) => ({ ...fd, name, surname, step: 2 }));
+    }
+    return console.log("Enter name and surname");
+  };
 
-  const setStep2 = (age, gender) =>
-    setFormData((fd) => ({ ...fd, age, gender, step: 3 }));
+  const setStep2 = (age, gender) => {
+    if (age !== "" && gender !== "") {
+      return setFormData((fd) => ({ ...fd, age, gender, step: 3 }));
+    }
+    return console.log("Enter age and gender");
+  };
 
-  const setStep3 = (companyName, companyCode) =>
-    setFormData((fd) => ({ ...fd, companyName, companyCode, step: 4 }));
+  const setStep3 = (companyName, companyCode) => {
+    if (companyName !== "" && companyCode !== "") {
+      return setFormData((fd) => ({
+        ...fd,
+        companyName,
+        companyCode,
+        step: 4,
+      }));
+    }
+    return console.log("Enter company name and code");
+  };
 
   const goPrevStep = () => setFormData((fd) => ({ ...fd, step: fd.step - 1 }));
 
